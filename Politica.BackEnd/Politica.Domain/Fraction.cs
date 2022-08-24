bcccb0db50abb697f758a3a6050de4be618c84e7
Adler <prop.het@mail.ru>
2022-08-24 15:35:26 +0300

    Added BackEnd Template

---
+++ Politica.BackEnd/Politica.Domain/Fraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Politica.Domain
+{
+    public class Fraction
+    {
+        Guid OwnerId { get; set; }
+        Guid Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Coordinates { get; set; }
+        IEnumerable<Player> ListPlayers { get; set; }
+    }
+}
---
+++ Politica.BackEnd/Politica.Domain/Players.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Politica.Domain
+{
+    public class Player
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+    }
+}
